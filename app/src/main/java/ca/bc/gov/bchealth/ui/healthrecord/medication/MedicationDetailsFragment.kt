92f9fd8075456af2f812d9bcb87620552bd971d9
rashmibambhania <rashmi@freshworks.io>
2022-05-03 15:36:14 +0530

    removed get auth params

--- app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/medication/MedicationDetailsFragment.kt
+++ app/src/main/java/ca/bc/gov/bchealth/ui/healthrecord/medication/MedicationDetailsFragment.kt
@@ -16,10 +16,10 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import ca.bc.gov.bchealth.R
 import ca.bc.gov.bchealth.databinding.FragmentMedicationDetailsBinding
 import ca.bc.gov.bchealth.ui.BaseFragment
+import ca.bc.gov.bchealth.ui.comment.CommentEntryTypeCode
 import ca.bc.gov.bchealth.utils.AlertDialogHelper
 import ca.bc.gov.bchealth.utils.updateCommentEndIcon
 import ca.bc.gov.bchealth.utils.viewBindings
-import ca.bc.gov.repository.CommentEntryTypeCode
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.launch
 
